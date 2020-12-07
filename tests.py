9455f0c8cc5df0ca16c73d84b9c8fdab30e554a2
Aman Sanger <amanrsanger@gmail.com>
2020-12-07 16:21:32 -0500

    Most up to date version of code

--- tests.py
+++ tests.py
@@ -1,15 +1,18 @@
 import torch
+import chess
 from model import NNUE
 import halfkp
 import nnue_dataset
 from data import fen_to_one_hot, output_stream
-from nnue_bin_dataset import NNUEBinData
+from nnue_bin_dataset import NNUEBinData, ToTensor
 from fen_to_bin import fen_to_bitvec
 
-b = '1sw3sw1ss/NE2se4/6sw1/1SE2sw3/3NE3ne/NE7/2NE4nw/EE3SW1NW1 W'
+b = 'rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2'
+bd = chess.Board(fen=b)
 y = 0
-out = fen_to_bitvec((b, y))
-
+#out = fen_to_bitvec((b, y))
+first = ToTensor()((bd, None, 0.5, 1))
+'''
 with open("test.bin", "wb") as f:
     for val in out:
         f.write(val)
@@ -18,7 +21,10 @@ val = NNUEBinData("test.bin")
 print(len(val))
 
 first = val.get_raw(0)
-model = torch.load("out.pt")
+'''
+checkpoint = torch.load("models/default/version_3/checkpoints/epoch=0-val_loss=0.05.ckpt")
+model = NNUE()
 print(first)
-
-print(int(model(*first[:4])*600))
+model.load_state_dict(checkpoint['state_dict'])
+print(int(model(first[0].reshape(1, -1), first[1].reshape(1, -1), \
+            first[2].reshape(1, -1), first[3].reshape(1, -1))*600))
